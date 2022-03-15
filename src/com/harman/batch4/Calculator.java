03f6b2eac5a1d91f29e164c2b370aec3b5880323
bibhukjha <msgbox.ubibhu@gmail.com>
2022-03-15 17:18:43 +0530

    Substraction classn

--- src/com/harman/batch4/Calculator.java
+++ src/com/harman/batch4/Calculator.java
@@ -4,23 +4,16 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public int Addition_s(int x, int y)
-    {
-        int z = x+y;
-        return z;
-    }
-
     public static void main(String[] args) {
 
-        Addition Calc=new Addition();
-        Substraction adCalc=new Substraction();
-        Calculator calc=new Calculator();
+
         Scanner input=new Scanner(System.in);
         int n1, n2,result1, result2;
         System.out.println("Enter n1: ");
         n1 = input.nextInt();
         System.out.println("Enter n2: ");
         n2 = input.nextInt();
+
         result1 = Calc.AdditionOp(n1,n2);
         result2 = adCalc.SubstractionOp(n1,n2);
         System.out.println("addition of n1 and n2 is: ");
---
+++ src/com/harman/batch4/ParentCalc.java
@@ -0,0 +1,16 @@
+package com.harman.batch4;
+
+public class ParentCalc {
+
+    public int Addition_add(int x, int y){
+        return x+y;
+    }
+
+    public int Substraction_sub(int x, int y){
+        return x-y;
+    }
+
+    public int Multiplication_mul(int x, int y){
+        return x*y;
+    }
+}
