0bac9edf0695ff4fc3f6cbc1d4ad3af992f7694f
bibhukjha <msgbox.ubibhu@gmail.com>
2022-03-15 16:18:37 +0530

    Calculatort

--- src/com/harman/batch4/Calculator.java
+++ src/com/harman/batch4/Calculator.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 
 public class Calculator {
 
+    public int Addition(int x, int y)
+    {
+        int z = x+y;
+        return z;
+    }
+
     public static void main(String[] args) {
 
+        Calculator calc=new Calculator();
         Scanner input=new Scanner(System.in);
         int n1, n2,result;
         System.out.println("Enter n1: ");
         n1 = input.nextInt();
         System.out.println("Enter n2: ");
         n2 = input.nextInt();
-        result = n1+n2;
+        result = calc.Addition(n1,n2);
         System.out.println(result);
 
-
     }
-
 
 }
